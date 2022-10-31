641dea9bd0ead3ac98c4dd2fb16d765e81b8772a
ManiMaran001 <cmmaran102@gmail.com>
2022-10-31 07:04:59 +0530

    cors

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app=express();
 const cors=require("cors");
 const vehicleRoutes=require("./routes/VehicleRoutes");
 const corsOptions ={
-    origin:'https://razocab.vercel.app/', 
-    credentials:true,          
-    optionSuccessStatus:200
-}
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api/v1',vehicleRoutes);
